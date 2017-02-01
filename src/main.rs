2fb7e212c14ed1df7610ba8d7407d2ea360693c7
Michael Shaw <michaeljshaw@gmail.com>
2017-02-01 19:20:37 +1100

    1024

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ use sponge::*;
 fn main() {
     println!("starting renderer");
 
-    let render_width = 512;
-    let render_height = 512;
+    let render_width = 1024;
+    let render_height = 1024;
 
-    let scale = 2;
+    let scale = 1;
 
     let window_width : u32 = render_width * scale;
     let window_height : u32 = render_height * scale;
@@ -22,11 +22,11 @@ fn main() {
     renderer.receive_channel.recv().unwrap(); // swallow the window ready message
 
     for i in 0..100000 {
-        let mut img = RgbaImage::from_pixel(render_width, render_height, Rgba { data: [0,0,0,255] });
+        let mut img = RgbaImage::from_pixel(render_width, render_height, Rgba { data: [(i % 256) as u8,0,0,255] });
         for x in 0..render_width {
             for y in 0..render_height {
-                let pixel : Rgba<u8> = Rgba { data: [(x % 256) as u8,(y % 256) as u8,(i % 256) as u8, 255] };
-                img.put_pixel(x, y, pixel);
+                // let pixel : Rgba<u8> = Rgba { data: [(x % 256) as u8,(y % 256) as u8,(i % 256) as u8, 255] };
+                // img.put_pixel(x, y, pixel);
             }
         }
 
