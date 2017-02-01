06a1da5a9597aedf181f44e4dc0a0b19e5cde677
Michael Shaw <michaeljshaw@gmail.com>
2017-02-01 19:42:54 +1100

    Fixed error handling

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,13 @@ use sponge::*;
 use sponge::RendererReply::*;
 use sponge::RendererUpdate::*;
 
-use std::num;
-
 fn main() {
     println!("starting renderer");
 
     let render_width = 512;
     let render_height = 512;
 
-    let scale = 1;
+    let scale = 2;
 
     let window_width : u32 = render_width * scale;
     let window_height : u32 = render_height * scale;
@@ -25,7 +23,9 @@ fn main() {
     
     renderer.receive_channel.recv().unwrap(); // swallow the window ready message
 
-    'outter: for i in 0..100000 {
+    let mut renderer_initiated_shutdown = false;
+
+    'outter: for i in 0..100 {
         'check_render: loop {
             match renderer.receive_channel.try_recv() {
                 Err(_) => {
@@ -38,22 +38,44 @@ fn main() {
                     }
                 },
                 Ok(WindowReady) => (),
-                Ok(RendererShutdown) => break 'outter,
+                Ok(RendererShutdown) => {
+                    renderer_initiated_shutdown = true;
+                    break 'outter
+                }
             }    
         }
         
         let mut img = RgbaImage::from_pixel(render_width, render_height, Rgba { data: [(i % 256) as u8,0,0,255] });
-        // for x in 0..render_width {
-        //     for y in 0..render_height {
-        //         let pixel : Rgba<u8> = Rgba { data: [(x % 256) as u8,(y % 256) as u8,(i % 256) as u8, 255] };
-        //         img.put_pixel(x, y, pixel);
-        //     }
-        // }
+        
+        text_pattern_render(i, &mut img);
 
-        renderer.send_channel.send(Render(i, img)).unwrap();
+        match renderer.send_channel.send(Render(i, img)) {
+            Ok(_) => (),
+            Err(err) => {
+                renderer_initiated_shutdown = true;
+                break 'outter;
+            },
+        }
     }
 
-    renderer.send_channel.send(ShutdownRenderer).unwrap();
+    if !renderer_initiated_shutdown {
+        renderer.send_channel.send(ShutdownRenderer).unwrap();    
+    }
     renderer.join_handle.join().unwrap()
 }
 
+fn sponge_renderer(n: u64, img: &mut RgbaImage) {
+    let (width, height) = img.dimensions();
+
+
+}
+
+fn text_pattern_render(n: u64, img: &mut RgbaImage) {
+    let (width, height) = img.dimensions();
+    for x in 0..width {
+        for y in 0..height {
+            let pixel : Rgba<u8> = Rgba { data: [(x % 256) as u8,(y % 256) as u8,(n % 256) as u8, 255] };
+            img.put_pixel(x, y, pixel);
+        }
+    }
+}
